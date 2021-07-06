ef9200f590fd87e96ffb1deecb8420e6a651ce79
Krzysztof Kura <kkura@concisesoftware.com>
2021-07-06 12:21:01 +0200

    [BB-34] added migration with user table

---
+++ src/migrations/1625563196521-addUserTable.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class AddUserTable1625563196521 implements MigrationInterface {
+  private tableName = 'user';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            length: '36',
+            isPrimary: true,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '320',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'datetime',
+            length: '6',
+            isNullable: false,
+            default: 'CURRENT_TIMESTAMP(6)',
+          },
+          {
+            name: 'updated_at',
+            type: 'datetime',
+            length: '6',
+            isNullable: true,
+            default: 'CURRENT_TIMESTAMP(6)',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
