a9d005e4b481ea328103f4c55f28367159f69fe2
Gabriel Ślawski <gslawski@concisesoftware.com>
2022-05-27 14:14:14 +0200

    [BM-382]: fix dependencies audit

--- audit.sh
+++ audit.sh
@@ -3,8 +3,8 @@
 # run yarn audit, check the number of lines that have 'high' on
 # them in the output table, meaning high vulnerability
 
-if [[ $(yarn audit | grep "high" |  wc -l | tr -d ' ') -gt 0 ]]; then
-   echo "high vulnerability found"
+if [[ $(yarn audit | grep "critical" |  wc -l | tr -d ' ') -gt 0 ]]; then
+   echo "critical vulnerability found"
    exit 1
  else
    exit 0
