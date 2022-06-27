b33f25d4014bc85e37008d8b7533762bb4f443b4
Gabriel Slawski <gslawski@concisesoftware.com>
2022-06-27 12:05:40 +0200

    encryption fixed on falsy values

--- src/common/helper/entity-encrypter.ts
+++ src/common/helper/entity-encrypter.ts
@@ -1,13 +1,19 @@
 import { Encrypter } from "./encrypter";
 
+const falsyValues = [undefined, null, ''];
+
 export const encryptEntity = (entity: any, columns: string[]) => {
     columns.forEach((column: string) => {
-        entity[column] = Encrypter.encrypt(`${entity[column]}`);
+        if (!falsyValues.includes(entity[column])) {
+            entity[column] = Encrypter.encrypt(`${entity[column]}`);
+        }
     });
 }
 
 export const decryptEntity = (entity: any, columns: string[]) => {
     columns.forEach((column: string) => {
-        entity[column] = Encrypter.decrypt(entity[column]);
+        if (!falsyValues.includes(entity[column])) {
+            entity[column] = Encrypter.decrypt(entity[column]);
+        }
     });
 }
