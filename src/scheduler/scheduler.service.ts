93828163b2ca0c690b4c79a47fa4192fa57f1941
meto a <meto.a@beyondbasics.co>
2025-08-25 14:35:57 +0000

    test commit #5

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -30,7 +30,7 @@ export class SchedulerService extends NestSchedule {
 
   @Cron("0 */5 * * * *")
   async informAboutTimeSlots() {
-      console.log("test logging. test, one more test");
+      console.log("test logging. test, one more test, test1");
     const slots = await this.triggerTimeSlotService.getSlotsToInform();
 
     for (let slot of slots) {
