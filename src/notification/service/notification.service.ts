20542950b97fe5c41031afd4061e1fe2dca6fd88
Metodija Antuleski <meto.a@beyondbasics.co>
2025-08-30 03:03:14 +0200

    :: added

--- src/notification/service/notification.service.ts
+++ src/notification/service/notification.service.ts
@@ -71,7 +71,7 @@ export class NotificationService {
       stopPropagation?: boolean;
     }
   ) {
-    this.logger.error(`Sms has not been sent (uid: ${params.userId})`);
+    this.logger.error(`Sms has not been sent:: (uid: ${params.userId})`);
     this.logger.error(JSON.stringify(error));
 
     if (params?.stopPropagation) {
