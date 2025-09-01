114d99195e60f702f6559c8cc060763ce3e976f4
Metodija Antuleski <meto.a@beyondbasics.co>
2025-09-01 12:14:29 +0200

    waking up cron on 15 mins for testing

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -143,20 +143,22 @@ export class SchedulerService extends NestSchedule {
     await this.triggerEmergencyMessage(false);
   }
 
-  @Cron('0 */2 * * * *')
-  async testCronLogging() {
-    const now = new Date().toISOString();
-    console.log(`Test Cron triggered at ${now}`);
-    this.logger.log(`Test Cron triggered at ${now}`);
+  // Leaving here for testing purposes
+  // @Cron('0 */2 * * * *')
+  // async testCronLogging() {
+  //   const now = new Date().toISOString();
+  //   console.log(`Test Cron triggered at ${now}`);
+  //   this.logger.log(`Test Cron triggered at ${now}`);
 
-    // Simulate some async work
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+  //   // Simulate some async work
+  //   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    console.log(`Test Cron finished at ${new Date().toISOString()}`);
-    this.logger.log(`Test Cron finished at ${new Date().toISOString()}`);
-  }
+  //   console.log(`Test Cron finished at ${new Date().toISOString()}`);
+  //   this.logger.log(`Test Cron finished at ${new Date().toISOString()}`);
+  // }
 
-  @Cron('0 0 * * * *')
+  // @Cron('0 0 * * * *')
+  @Cron('0 */15 * * * *')
   async wakeUpAppGeneric() {
     const allProfiles = await this.profileRepository.findAllActiveDeviceIds();
 
