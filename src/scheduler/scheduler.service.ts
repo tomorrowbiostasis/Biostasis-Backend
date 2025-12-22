973dbe3034b84422d72120290a5d294b233b3279
Metodija Antuleski <meto.a@beyondbasics.co>
2025-12-22 23:31:01 +0100

    remove commented-out checkNotRegularPositiveInfo method from SchedulerService

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -137,15 +137,15 @@ export class SchedulerService extends NestSchedule {
     }
   }
 
-  @Cron("0 */5 * * * *")
-  async checkNotRegularPositiveInfo() {
-    console.log('Console log from SchedulerService at checkNotRegularPositiveInfo', new Date().toISOString());
-    this.logger.log("Test log entry from SchedulerService at checkNotRegularPositiveInfo" + new Date().toISOString());
-    await this.sendPushNotificationDueToLackOfPositiveInfo();
-    await this.sendSmsDueToLackOfPositiveInfo(false);
-    await this.sendAlertDueToLackOfPositiveInfo(false);
-    await this.triggerEmergencyMessage(false);
-  }
+  // @Cron("0 */5 * * * *")
+  // async checkNotRegularPositiveInfo() {
+  //   // console.log('Console log from SchedulerService at checkNotRegularPositiveInfo', new Date().toISOString());
+  //   // this.logger.log("Test log entry from SchedulerService at checkNotRegularPositiveInfo" + new Date().toISOString());
+  //   await this.sendPushNotificationDueToLackOfPositiveInfo();
+  //   await this.sendSmsDueToLackOfPositiveInfo(false);
+  //   await this.sendAlertDueToLackOfPositiveInfo(false);
+  //   await this.triggerEmergencyMessage(false);
+  // }
 
   @Cron('0 */20 * * * *')
   async wakeUpAppGeneric() {
