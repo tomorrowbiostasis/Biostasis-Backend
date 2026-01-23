0ce6c9ab8e73095fe030c3b5c0e57cfd54a553b3
Metodija Antuleski <meto.a@beyondbasics.co>
2026-01-23 12:38:30 +0100

    refactor: uncomment schedulerService import and restore sendPushNotificationForSingleUser logic in TriggerEmergencyController

--- src/user/controller/trigger-emergency.controller.ts
+++ src/user/controller/trigger-emergency.controller.ts
@@ -37,7 +37,7 @@ export class TriggerEmergencyController {
         private readonly userService: UserService,
         private readonly profileRepository: ProfileRepository,
         private readonly notificationService: NotificationService,
-        // private readonly schedulerService: SchedulerService,
+        private readonly schedulerService: SchedulerService,
     ) { }
 
     @ApiResponse({ status: 200, type: SuccessRO })
@@ -54,13 +54,14 @@ export class TriggerEmergencyController {
             };
         }
 
-        // const result = await this.schedulerService.sendPushNotificationForSingleUser(
-        //     user.id,
-        //     profile.user.deviceId,
-        // );
+        const result = await this.schedulerService.sendPushNotificationForSingleUser(
+            user.id,
+            profile.user.deviceId,
+        );
 
         return {
-            success: 'true'
+            success: 'true',
+            data: result,
         };
         // await this.notificationService.sendEmergencyMessage(
         //     {
